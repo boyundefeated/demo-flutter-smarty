2bfddc1d3d1c159d546aa09b78f01c1412860865
Parabeac-Nest <admin@parabeac.com>
2022-11-01 04:18:43 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/smarty_colors.g.dart
@@ -0,0 +1,34 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class SmartyColors {
+
+static const Color grayscaleBody = Color(0xff4e4b66);
+
+static const Color neutralGrey = Color(0xff9098b1);
+
+static const Color content1 = Color(0xffd32845);
+
+static const Color primary6 = Color(0xff1890ff);
+
+static const Color characterPrimary(inverse) = Color(0xffffffff);
+
+static const Color neutral6 = Color(0xffbfbfbf);
+
+static const Color contentGrey3 = Color(0xff626262);
+
+static const Color primary1 = Color(0xff299274);
+
+static const Color systemIcon = Color(0xffb5b8cb);
+
+static const Color contentGrey4 = Color(0xff202223);
+
+static const Color statusError = Color(0xffcb2d31);
+
+static const Color grayscaleWhite = Color(0xffffffff);
+}
